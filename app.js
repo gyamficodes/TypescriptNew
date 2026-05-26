b9071c4eb4ec9f6ccf3984e70adcc88787e5583e
gyamficodes <gyamfijohn08@gmail.com>
2026-05-26 18:59:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+// JavaScript and TypeScript Primitives
+//boolean, number, string, null, undefined, symbol, bigint
+let isActive = true;
+let age = 30;
+let names = "John Doe";
+let nullableValue = null;
+let undefinedValue = undefined;
+let uniqueId = Symbol("id");
+let bigIntValue = 9007199254740991n;
+console.log(isActive); // true
+console.log(age);
+//TypeScript Explicit Types and Inference
+let name1 = "Alice";
+console.log(name1); // Alice
+let logedIn = false;
+console.log(logedIn); // false
---
+++ app.ts
@@ -0,0 +1,22 @@
+// JavaScript and TypeScript Primitives
+//boolean, number, string, null, undefined, symbol, bigint
+
+let isActive: boolean = true;
+let age: number = 30;
+let names: string = "John Doe";
+let nullableValue: null = null;
+let undefinedValue: undefined = undefined;
+let uniqueId: symbol = Symbol("id");
+let bigIntValue: bigint = 9007199254740991n;
+
+console.log(isActive); // true
+console.log(age);
+
+
+//TypeScript Explicit Types and Inference
+
+let name1: string = "Alice";
+console.log(name1); // Alice
+
+let logedIn: boolean = false;
+console.log(logedIn); // false
